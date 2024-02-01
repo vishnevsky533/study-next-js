d67872b1eb28676d6a6917b23aa8a1645c8dbe5a
vishnevsky533 <vishnevsky533@gmail.com>
2024-02-01 17:26:05 +0300

    first commit

---
+++ app/client/page.js
@@ -0,0 +1,5 @@
+export default function Client() {
+    return (
+        <h1>Client страница</h1>
+    );
+}
---
+++ app/page.js
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <h1>Главная страница</h1>
+  );
+}
